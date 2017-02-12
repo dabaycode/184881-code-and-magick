a0524e5af461f757aa247e8cf164c1ff961953ee
Dmitry Bayko <special.dima@gmail.com>
2017-02-12 23:17:54 +1000

    Сделал одну функцию

--- js/setup.js
+++ js/setup.js
@@ -29,28 +29,68 @@ var fireballColors = [
   '#e6e848'
 ];
 
+/**
+ * anonymous function - Возвращает индекс рандомного элемента массива.
+ *
+ * @param  {array} colorsArray - входной массив.
+ * @return {number} - индекс ранжомного элемента массива.
+ */
+var randomColorIndex = function (colorsArray) {
+  return (Math.floor(Math.random() * colorsArray.length));
+};
+
+
+/**
+ * randomColor - меняет фоновый цвет элемента.
+ *
+ * @param  {object} changedNode - входной элемент.
+ * @param  {array} colorsArray - входной массив цветов.
+ * @param  {string} prop - значение изменяего свойства.
+ * @param  {object} event - принимает событие click и связанные объекты.
+ * @return {undefined} - выход из функции.
+ */
+function randomColor(changedNode, colorsArray, prop, event) {
+  if (prop === 'fill') {
+    changedNode.style.fill = colorsArray[randomColorIndex(colorsArray)];
+  }
+  if (prop === 'background') {
+    changedNode.style.background = colorsArray[randomColorIndex(colorsArray)];
+  }
+
+  return;
+}
+
+
+/**
+ * anonymous function - открывает окно персонажа. Удаляет класс invisible.
+ *
+ */
 setupOpen.addEventListener('click', function () {
   setup.classList.remove('invisible');
 });
 
+
+/**
+ * anonymous function - закрывает окно персонажа. Добавляет класс invisible.
+ *
+ * @return {type}  description
+ */
 setupClose.addEventListener('click', function () {
   setup.classList.add('invisible');
 });
 
-wizardCoat.addEventListener('click', function () {
-  wizardCoat.style.fill = wizardCoatColors[
-    Math.floor(Math.random() * wizardCoatColors.length)
-  ];
-});
 
-wizardEyes.addEventListener('click', function () {
-  wizardEyes.style.fill = wizardEyesColors[
-    Math.floor(Math.random() * wizardEyesColors.length)
-  ];
-});
+/**
+ * Вызывает функцию randomColor при событии click на элемент с #wizardCoat.
+ */
+wizardCoat.addEventListener('click', randomColor.bind(null, wizardCoat, wizardCoatColors, 'fill'));
 
-setupFireball.addEventListener('click', function () {
-  setupFireball.style.background = wizardCoatColors[
-    Math.floor(Math.random() * fireballColors.length)
-  ];
-});
+/**
+ * Вызывает функцию randomColor при событии click на элемент с #wizardEyes.
+ */
+wizardEyes.addEventListener('click', randomColor.bind(null, wizardEyes, wizardEyesColors, 'fill'));
+
+/**
+ * Вызывает функцию randomColor при событии click на элемент c .setup-fireball-wrap
+ */
+setupFireball.addEventListener('click', randomColor.bind(null, setupFireball, fireballColors, 'background'));
